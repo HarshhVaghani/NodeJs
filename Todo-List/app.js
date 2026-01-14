02c0f2207a80ccf0f901d6433b19f0267d8fdd60
Vaghaniharshh <hvaghani89@gmail.com>
2026-01-14 19:34:16 +0530

    14-1-26

---
+++ Todo-List/app.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// In-memory storage
+let todos = [];
+
+// Home route
+app.get("/", (req, res) => {
+  res.render("index", { todos });
+});
+
+// Add task
+app.post("/add", (req, res) => {
+  const task = req.body.task;
+
+  if (task && task.trim() !== "") {
+    todos.push(task.trim());
+  }
+  res.redirect("/");
+});
+
+// Edit task
+app.post("/edit/:index", (req, res) => {
+  const index = req.params.index;
+  const updatedTask = req.body.updatedTask;
+
+  if (updatedTask && updatedTask.trim() !== "") {
+    todos[index] = updatedTask.trim();
+  }
+  res.redirect("/");
+});
+
+// Delete task
+app.post("/delete/:index", (req, res) => {
+  todos.splice(req.params.index, 1);
+  res.redirect("/");
+});
+
+// Server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
